101b0d87f423afcc84a62678ec2b9931a5d2d4b6
nasarinagasunil <nagasunilnasari@gmail.com>
2025-09-17 15:01:15 +0530

    memo

---
+++ memo/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, memo } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const Greeting = memo(function Greeting({ name }) {
+  console.log('Greeting render')
+  return <h1>Hello, {name}!</h1>
+})
+
+function App() {
+  const [name, setName] = useState('')
+  const [city, setCity] = useState('')
+  return (
+    <>
+      <div>
+        <input onChange={(e) => setName(e.target.value)} type="text" placeholder='Name...'/>
+      </div>
+      <div>
+        <input onChange={(e) => setCity(e.target.value)} type="text" placeholder='City...'/>
+      </div>
+      <Greeting name={name} />
+
+    </>
+  )
+}
+
+export default App
